2b59ae4b5d220eb843e9328ab196ecf08d4fe650
devInventam <dev@inventam.com>
2026-04-09 10:48:40 +0530

    Add loading component and refactor Home page to use async data fetching
    - Introduced a new Loading component with a preloader using framer-motion.
    - Updated the Home page to fetch projects asynchronously and pass the data as props.
    - Replaced the use of hooks for data fetching with direct service calls for improved performance.
    - Switched to dynamic imports for Home components to enhance loading efficiency.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
-"use client";
-
 import Home from "@/screens/Home";
+import { getProjects } from "@/services/projectService";
 
-export default function Page() {
-  return <Home />;
+export default async function Page() {
+  const projectsResponse = await getProjects({
+    page: 1,
+    limit: 5,
+    sort_column: "project_home_sequence",
+    sort_order: "asc",
+    show_on_home_page: true,
+  });
+
+  return <Home projectsResponse={projectsResponse} />;
 }
